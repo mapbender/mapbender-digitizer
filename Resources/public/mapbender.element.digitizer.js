3e7b95b1a9b1290ff23531a184db7fbf8c303a0b
Axel Schaefer <axel.schaefer@wheregroup.com>
2017-03-07 14:36:55 +0100

    Update to get the option from the schema
    This code gets the option from the schema. The "allowDeleteByCancelNewGeometry" is set to true but not available in the options but in the schema.

--- Resources/public/mapbender.element.digitizer.js
+++ Resources/public/mapbender.element.digitizer.js
@@ -940,7 +940,7 @@
                         var dialog = $(this).closest(".ui-dialog-content");
                         var olFeature = dialog.data('feature');
                         var options = widget.options;
-                        if(olFeature.hasOwnProperty('isNew') && options.allowDeleteByCancelNewGeometry) {
+                        if(olFeature.hasOwnProperty('isNew') && schema.allowDeleteByCancelNewGeometry) {
                             widget.removeFeature(olFeature);
                         }
                         widget.currentPopup.popupDialog('close');
