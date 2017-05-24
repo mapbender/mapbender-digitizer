36383bf3d07e2f1c9f781d6a68b1e9886ea0a56f
wg-rolf <rolf.neuberger@wheregroup.com>
2017-05-24 16:49:33 +0200

    Fixed regression: deactivate did not hide feature layer when no unsaved features

--- Resources/public/mapbender.element.digitizer.js
+++ Resources/public/mapbender.element.digitizer.js
@@ -1956,10 +1956,10 @@
             var unsavedFeatures = widget.unsavedFeatures;
             widget.unsavedFeatures = {};
             var always = function() {
+                widget.options.__disabled = true;
                 if(!widget.currentSettings.displayOnInactive) {
                     widget.deactivateFrame(widget.currentSettings);
                 }
-                widget.options.__disabled = true;
             };
             if (widget.options.confirmSaveOnDeactivate) {
                 widget._confirmSave(unsavedFeatures, always);
@@ -2010,6 +2010,8 @@
                     }
                 };
                 Mapbender.confirmDialog(confirmOptions);
+            } else {
+                callback();
             }
         },
 
