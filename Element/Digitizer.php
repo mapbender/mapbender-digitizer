f924b77ef85ecf7c51e9137dc9d8f56e8b47d385
wg-rolf <rolf.neuberger@wheregroup.com>
2019-01-15 17:18:57 +0100

    Bring your own toolset

--- Element/Digitizer.php
+++ Element/Digitizer.php
@@ -33,6 +33,7 @@ class Digitizer extends BaseElement
                 '../../vendor/blueimp/jquery-file-upload/js/jquery.fileupload.js',
                 '../../vendor/blueimp/jquery-file-upload/js/jquery.iframe-transport.js',
                 "/components/jquery-context-menu/jquery-context-menu-built.js",
+                '@MapbenderDigitizerBundle/Resources/public/digitizingToolset.js',
                 '@MapbenderDigitizerBundle/Resources/public/mapbender.element.digitizer.js',
             ),
              'css' => array(
