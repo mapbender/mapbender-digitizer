31081b25794837a3dd27a9b07eb160e3a0f1eaee
inachtigall <igor.nachtigall@wheregroup.com>
2019-03-20 18:45:36 +0100

    remove unnecessary hasOwnProperty check

--- Resources/public/mapbender.element.digitizer.js
+++ Resources/public/mapbender.element.digitizer.js
@@ -3308,7 +3308,7 @@
             if(eventData.origin === 'cancel-button'){
                 this.currentPopup.popupDialog('close');
             }
-            if(!feature.hasOwnProperty('isNew') && feature.hasOwnProperty('oldGeom') && feature.oldGeom){
+            if(!feature.hasOwnProperty('isNew') && feature.oldGeom){
 
 
                 feature.geometry.x =  feature.oldGeom.x;
