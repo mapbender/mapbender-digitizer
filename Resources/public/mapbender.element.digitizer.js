f735043ce0508ce7f80124eff0389d3383a43ba8
Rauch <=>
2019-04-15 14:10:03 +0200

    Correct bug on copying from dialogue

--- Resources/public/mapbender.element.digitizer.js
+++ Resources/public/mapbender.element.digitizer.js
@@ -1106,7 +1106,7 @@
                     click: function (e) {
                         var dialog = $(this).closest('.ui-dialog-content');
                         var feature = dialog.data('feature');
-                        widget.copyFeature(olFeature); // TODO possibly a bug?
+                        widget.copyFeature(feature);
                     }
                 });
             }
