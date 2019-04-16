e8ab9118f9f640301972a0a9f40246907d2646ce
Rauch <=>
2019-04-16 14:34:47 +0200

    Refactor FormItemResultTable 3

--- Resources/public/formItems/formItemFile.js
+++ Resources/public/formItems/formItemFile.js
@@ -3,6 +3,27 @@
 
     window.FormItemFile = {
 
+        process: function(feature) {
+            var item = this.clone();
+            var schema = this.schema;
+
+            item.uploadHanderUrl = schema.widget.elementUrl + "file/upload?schema=" + schema.schemaName + "&fid=" + feature.fid + "&field=" + item.name;
+            if (item.hasOwnProperty("name") && feature.data[item.name]) {
+                item.dbSrc = feature.data[item.name];
+                if (schema.featureType.files) {
+                    schema.featureType.files.forEach( function (fileInfo) {
+                        if (fileInfo.field === item.name) {
+                            if (fileInfo.formats) {
+                                item.accept = fileInfo.formats;
+                            }
+                        }
+                    });
+                }
+            }
+
+            return item;
+
+        }
     };
 
     Object.setPrototypeOf(FormItemFile, FormItem);
--- Resources/public/formItems/formItemImage.js
+++ Resources/public/formItems/formItemImage.js
@@ -3,6 +3,35 @@
 
     window.FormItemImage = {
 
+        process: function(feature) {
+            var item = this.clone();
+            var schema = item.schema;
+            var widget = schema.widget;
+
+            if (!item.origSrc) {
+                item.origSrc = item.src;
+            }
+
+            if (item.name && feature.data[item.name]) {
+                item.dbSrc = feature.data[item.name];
+                if (schema.featureType.files) {
+                    schema.featureType.files.forEach(function (fileInfo) {
+                        if (fileInfo.field === item.name) {
+                            if (fileInfo.uri) {
+                                item.dbSrc = fileInfo.uri + "/" + item.dbSrc;
+                            } else {
+                                item.dbSrc = widget.options.fileUri + "/" + schema.featureType.table + "/" + item.name + "/" + item.dbSrc;
+                            }
+                        }
+                    });
+                }
+            }
+
+            var src = item.dbSrc || item.origSrc;
+            item.src = item.relative ? Mapbender.configuration.application.urls.asset + src : src;
+            return item;
+        }
+
     };
 
     Object.setPrototypeOf(FormItemImage, FormItem);
