a043b2af6b80ea99b4375c5289dbbcadd231ab62
Felix Rauch <felix.rauch@wheregroup.com>
2020-02-27 16:40:12 +0100

    Revert last commit

--- Resources/public/featureStyleEditor.js
+++ Resources/public/featureStyleEditor.js
@@ -526,10 +526,10 @@
         // tabs.push(backGroundTab);
         // tabs.push(miscTab);
 
-        element.generateElements([{
+        element.generateElements({
             type: "tabs",
             children: tabs
-        }]);
+        ]);
 
         element.popupDialog({
             title: "Stylemanager",
