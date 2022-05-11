bdbc4a1f846a6bd36f6281366895247146b59d59
werrolf <rolf.neuberger@wheregroup.com>
2022-05-11 13:35:19 +0200

    Supply item primary id on dialog attribute data-item-id

--- src/Mapbender/DataManagerBundle/Resources/public/dataManager.element.js
+++ src/Mapbender/DataManagerBundle/Resources/public/dataManager.element.js
@@ -409,6 +409,7 @@
             var dialog = $("<div/>");
             dialog.data('item', dataItem);
             dialog.data('schema', schema);
+            dialog.attr('data-item-id', this._getUniqueItemId(dataItem));
             dialog.append(this.formRenderer_.renderElements(schema.formItems));
             if (!schema.allowEdit) {
                 $('.-fn-delete-attachment', dialog).remove();
