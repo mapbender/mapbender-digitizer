1b960c768fc1c21d3c71b139ce311d8148eec5cb
werrolf <rolf.neuberger@wheregroup.com>
2022-12-07 11:05:56 +0100

    Add formal default for new "roles" schema setting

--- src/Mapbender/DataManagerBundle/Component/SchemaFilter.php
+++ src/Mapbender/DataManagerBundle/Component/SchemaFilter.php
@@ -59,6 +59,7 @@ class SchemaFilter
                 'searching' => true,
                 'pageLength' => 16,
             ),
+            'roles' => null,
             'listed' => true,
         );
     }
