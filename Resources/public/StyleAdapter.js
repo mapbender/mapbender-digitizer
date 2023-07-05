8f5f4572be4febf39f47fbc2fa874468cc1711fc
frauch <felix.rauch@wheregroup.com>
2023-07-05 14:43:48 +0200

    add labeloutlinecolor to style adapter

--- Resources/public/StyleAdapter.js
+++ Resources/public/StyleAdapter.js
@@ -119,6 +119,19 @@
             var textStyle = this.getDefaultTextStyle();
             textStyle.setFont(this.canvasFontRuleFromSvg(ol2Style));
             this.resolveTextStyle_(textStyle, ol2Style);
+
+            if (ol2Style.labelOutlineColor) {
+                var rgb = Mapbender.StyleUtil.parseCssColor(ol2Style.labelOutlineColor).slice(0, 3);
+                var opacity = (typeof ol2Style.labelOutlineOpacity !== 'undefined') ? parseFloat(ol2Style.labelOutlineOpacity) : 1;
+                if (!isNaN(opacity)) {
+                    rgb.push(opacity);
+                }
+                textStyle.setStroke(new ol.style.Stroke({
+                    color: rgb,
+                    width: ol2Style.labelOutlineWidth || 1
+                }));
+            }
+
             return textStyle;
         },
         getIconStyle: function(styleConfig) {
