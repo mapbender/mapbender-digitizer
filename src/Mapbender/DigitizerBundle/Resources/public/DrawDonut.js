22efc78de8737e9d6b6ce6e9334ab3be8d9632a6
frauch <felix.rauch@wheregroup.com>
2024-02-09 10:05:54 +0100

    ensure ol7 Compatibility of DrawDonut

--- src/Mapbender/DigitizerBundle/Resources/public/DrawDonut.js
+++ src/Mapbender/DigitizerBundle/Resources/public/DrawDonut.js
@@ -1,22 +1,21 @@
-;!(function() {
+;(function() {
     "use strict";
-    window.Mapbender = Mapbender || {};
-    window.Mapbender.Digitizer = Mapbender.Digitizer || {};
-    window.Mapbender.Digitizer.Interactions = Mapbender.Digitizer.Interactions || {};
+    window.Mapbender = window.Mapbender || {};
+    window.Mapbender.Digitizer = window.Mapbender.Digitizer || {};
+    window.Mapbender.Digitizer.Interactions = window.Mapbender.Digitizer.Interactions || {};
 
-    function DrawDonut(options) {
-        this.originalFeature_ = null;
-        ol.interaction.Draw.apply(this, [options]);
-    }
-    DrawDonut.prototype = Object.create(ol.interaction.Draw.prototype);
-    Object.assign(DrawDonut.prototype, {
-        constructor: DrawDonut,
-        startDrawing_: function() {
-            ol.interaction.Draw.prototype.startDrawing_.apply(this, arguments);
-            var map = this.getMap();
-            var coordinate = this.sketchFeature_.getGeometry().getFirstCoordinate();
+    class DrawDonut extends ol.interaction.Draw {
+        constructor(options) {
+            super(options);
+            this.originalFeature_ = null;
+        }
 
-            this.originalFeature_ = map.forEachFeatureAtPixel(map.getPixelFromCoordinate(coordinate), function (feature) {
+        startDrawing_() {
+            super.startDrawing_(...arguments);
+            const map = this.getMap();
+            const coordinate = this.sketchFeature_.getGeometry().getFirstCoordinate();
+
+            this.originalFeature_ = map.forEachFeatureAtPixel(map.getPixelFromCoordinate(coordinate), (feature) => {
                 if (feature.getGeometry().getType() === 'Polygon') {
                     return feature;
                 }
@@ -27,11 +26,13 @@
             } else {
                 this.dispatchEvent({type: 'modifystart', features: new ol.Collection([this.originalFeature_])});
             }
-        },
-        finishDrawing: function() {
-            var sketchFeature = this.abortDrawing_();
-            var coordinates = this.sketchCoords_;
-            var geometry = /** @type {ol.geom.SimpleGeometry} */ (sketchFeature.getGeometry());
+        }
+
+        finishDrawing() {
+            const sketchFeature = this.abortDrawing_();
+            let coordinates = this.sketchCoords_;
+            const geometry = sketchFeature.getGeometry();
+
             if (this.mode_ === ol.interaction.Draw.Mode_.LINE_STRING) {
                 // remove the redundant last point
                 coordinates.pop();
@@ -51,13 +52,14 @@
             } else if (this.type_ === ol.geom.GeometryType.MULTI_POLYGON) {
                 sketchFeature.setGeometry(new ol.geom.MultiPolygon([coordinates]));
             }
+
             if (this.originalFeature_) {
-                var ringCoordinates = sketchFeature.getGeometry().getCoordinates()[0];
+                const ringCoordinates = sketchFeature.getGeometry().getCoordinates()[0];
                 this.originalFeature_.getGeometry().appendLinearRing(new ol.geom.LinearRing(ringCoordinates));
                 this.dispatchEvent({type: 'modifyend', features: new ol.Collection([this.originalFeature_])});
             }
         }
-    });
+    }
 
     window.Mapbender.Digitizer.Interactions.DrawDonut = DrawDonut;
 }());
