efe9f79a60874c3d793b12fb836f58dc64d07bb7
Arno Fleckenstein <arno.fleckenstein@wheregroup.com>
2024-04-25 13:35:40 +0200

    change color of file upload button

--- src/Mapbender/DataManagerBundle/Resources/public/FormRenderer.js
+++ src/Mapbender/DataManagerBundle/Resources/public/FormRenderer.js
@@ -379,7 +379,7 @@
             var $btnText = $('<span class="upload-button-text">')
                 .attr('data-placeholder', settings.text || 'Select')
             ;
-            var $btn = $('<button class="btn btn-success button fileinput-button">')
+            var $btn = $('<button class="btn btn-sm btn-light fileinput-button">')
                 .append($fileInput)
                 .append($inputReal)
                 .append('<i class="fa fa-upload" aria-hidden="true"/>')
