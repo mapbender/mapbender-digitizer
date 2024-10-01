6a7757d21062ace05f700db5b3985b598863e944
frauch <felix.rauch@wheregroup.com>
2024-10-01 10:25:58 +0200

    enable update-multiple for non-existing features

--- src/Mapbender/DigitizerBundle/Resources/public/mapbender.element.digitizer.js
+++ src/Mapbender/DigitizerBundle/Resources/public/mapbender.element.digitizer.js
@@ -436,8 +436,8 @@
          * @return {string}
          * @private
          */
-        _generateNamespacedId: function(schema, feature) {
-            return [this.element.attr('id'), schema.schemaName, this._getUniqueItemId(feature)].join('-');
+        _generateNamespacedId: function(schema, feature, index = null) {
+            return [this.element.attr('id'), schema.schemaName, this._getUniqueItemId(feature), ...(index !== null ? [index] : [])].join('-');
         },
         _afterSave: function(schema, feature, originalId, responseData) {
             // unravel dm-incompatible response format
@@ -512,7 +512,7 @@
                 var feature = features[i];
                 var itemSchema = this.getItemSchema(feature);
                 continueDrawing = continueDrawing || itemSchema.continueDrawingAfterSave;
-                var mapId = this._generateNamespacedId(itemSchema, feature);
+                var mapId = this._generateNamespacedId(itemSchema, feature, i);
                 featureMap[mapId] = feature;
                 postData.features.push({
                     schemaName: itemSchema.schemaName,
@@ -536,7 +536,7 @@
                         widget._replaceItemData(itemSchema, feature, savedItem.properties || {});
                         feature.set('dirty', false);
                         feature.set('oldGeometry', geometry);
-                        widget.tableRenderer.refreshRow(feature, false);
+                        widget.tableRenderer.addOrRefreshRow(feature, true);
                         widget._saveEvent(itemSchema, feature, widget._getUniqueItemId(feature));
                     }
                     $.notify(Mapbender.trans('mb.data.store.save.successfully'), 'info');
